c8561d60ca80c3640c9cf9c3b3ba49ed25c9d08a
Kylie West <kmichellewest@gmail.com>
2023-05-27 12:30:37 -0500

    Try updating req headers

--- src/pages/api/rate.js
+++ src/pages/api/rate.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
   axios
     .get(`http://api.github.com/rate_limit`, {
       headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
         Authorization: token,
       },
     })
