7ecaa6453c932b6b224dde1d141d00213c76f1ef
Kylie West <kmichellewest@gmail.com>
2023-05-27 17:49:20 -0500

    Fix

--- src/pages/api/rate.js
+++ src/pages/api/rate.js
@@ -3,19 +3,15 @@ import axios from "axios";
 const token = "token" + " " + process.env.TOKEN;
 
 export default async function handler(req, res) {
-  if (!token) {
-    res.send("No token found");
-    return;
-  }
-  //   axios
-  //     .get(`http://api.github.com/rate_limit`, {
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //         Authorization: token,
-  //       },
-  //     })
-  //     .then(({ data }) => res.json(data));
+  // axios
+  //   .get(`http://api.github.com/rate_limit`, {
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       Authorization: token,
+  //     },
+  //   })
+  //   .then(({ data }) => res.json(data));
 
   fetch("http://api.github.com/rate_limit", {
     method: "GET",
@@ -24,5 +20,17 @@ export default async function handler(req, res) {
       "Content-Type": "application/json",
       Authorization: token,
     },
-  }).then(({ data }) => res.json(data));
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
 }
